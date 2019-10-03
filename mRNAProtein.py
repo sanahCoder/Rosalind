977d9745ebff5d2e72c75be2f4d8535dafd96d71
sanah-imani <sanah.imani@gmail.com>
2019-10-03 20:32:21 +0530

    Update mRNAProtein.py

--- mRNAProtein.py
+++ mRNAProtein.py
@@ -1,3 +1,5 @@
+http://rosalind.info/problems/prot/
+
 codonAA = {}
 aminoAcid = ['A','C','D','E','F','G','H','I','K','L','M','N','P','Q','R','S','T','V','W','Y']
 numCodons = [4,2,2,2,2,4,2,3,2,6,1,2,4,2,6,6,4,4,1,2]
